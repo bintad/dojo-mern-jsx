c4b9b3e943737f36180b187a5d03eaac5d9b5aab
binta diallo <bintadalen2009@gmail.com>
2020-05-11 12:10:55 -0700

    my jsx dojo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dojo from './conponents/Dojo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dojo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conponents/Dojo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Dojo = () => {
+    // Here we simply created a function that returns a new React element
+    return (
+        <>
+        <h1>Hello Dojo</h1>
+
+        <p>learn React</p>
+
+        <p>Climb Mt Everest</p>
+
+        <p>Run a marathon</p>
+
+        <p>Feed the dogs</p>
+        </>
+    );
+}
+export default Dojo;
